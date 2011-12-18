40dab66cd9a9fef811db3de81f4899dc5c2ef5a6
Roger <rog.m.davis@gmail.com>
2011-12-18 22:35:44 +0000

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,19 @@
 class PagesController < ApplicationController
+
+
+@roger= "Roger Davis"
+
   def home
+  	@title= "Home"
   end
 
   def contact
+  	@title= "Contact"
   end
+
+
+def about
+	@title= "About"
+end
 
 end
